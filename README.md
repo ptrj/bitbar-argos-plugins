46463f7c18aadb186634e341775426b027b8d42f
Peter Jirásek <pj@pj-mac.local>
2019-03-13 19:05:11 +0100

    multiple firefox profiles

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# bitbar-plugins
+# BitBar
+https://github.com/matryer/bitbar
+Put anything in your Mac OS X menu bar
+
+# Multiple Firefox profiles
+Run multiple Firefox profiles
+
+  - open Firefox
+  - in to URL type about:profiles
+  - create profiles
+  - put your profile names in to PROFILES variable
---
+++ multiple_ff_profiles.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# <bitbar.title>Multiple Firefox Profiles</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Peter Jirasek</bitbar.author>
+# <bitbar.author.github>ptrj</bitbar.author.github>
+# <bitbar.desc>You can open different Firefox profiles at the same time</bitbar.desc>
+# <bitbar.image>https://i.imgur.com/XxVNUa6.png</bitbar.image>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+
+## 1. open Firefox
+## 2. in to URL type about:profiles
+## 3. create profiles
+## 4. put your profile names in to PROFILES variable
+PROFILES="Default Anonimize Private Bank Work"
+
+ROOTDIR="$(dirname "${BASH_SOURCE[0]}")"
+SELFNAME="$(basename "${BASH_SOURCE[0]}")"
+PATTERN="+($(echo ${PROFILES// /|}))"
+EDITIONS="s e"
+
+
+## Firefox Stable
+open_ffs() {
+  /Applications/Firefox.app/Contents/MacOS/firefox -P "${PROFILE}" >/dev/null 2>&1 &
+}
+
+## Firefox ESR
+open_ffe() {
+  /Applications/Firefox\ ESR.app/Contents/MacOS/firefox -P "${PROFILE}" >/dev/null 2>&1 &
+}
+
+bitbar() {
+  echo "🦊"
+  echo "---";
+  for PROFILE in ${PROFILES}; do
+    echo "$PROFILE | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=s terminal=false"
+    echo "----"
+    echo "-- Stable | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=s terminal=false"
+    echo "-- ESR | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=e terminal=false"
+  done
+}
+
+EDITION="${2:-s}"
+PROFILE="$1"
+case "$PROFILE" in
+  $PATTERN)
+    "open_ff"${EDITION} $PROFILE
+    ;;
+    *) bitbar
+esac
