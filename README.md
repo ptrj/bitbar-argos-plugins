e7d1531c5569045813aba5257d7587eac3a02cc2
Peter Jirásek <pj@pj-mac.local>
2019-07-17 17:05:30 +0200

    update: compatibility with Argos & detect FF profiles & FF Editions

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # BitBar
 https://github.com/matryer/bitbar
 Put anything in your Mac OS X menu bar
+# Argos
+https://extensions.gnome.org/extension/1176/argos/
+Linux BitBar Alternative (GNOME Shell Extension)
 
 # Multiple Firefox profiles
 Run multiple Firefox profiles
 
   - open Firefox
-  - in to URL type about:profiles
+  - in to address bar type: about:profiles
   - create profiles
-  - put your profile names in to PROFILES variable
+  - install script
 
 ![Image](https://i.imgur.com/XxVNUa6.png)
--- multiple_ff_profiles.sh
+++ multiple_ff_profiles.sh
@@ -1,53 +1,159 @@
-#!/bin/bash
-#
-# <bitbar.title>Multiple Firefox Profiles</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
-# <bitbar.author>Peter Jirasek</bitbar.author>
-# <bitbar.author.github>ptrj</bitbar.author.github>
-# <bitbar.desc>You can open different Firefox profiles at the same time</bitbar.desc>
-# <bitbar.image>https://i.imgur.com/XxVNUa6.png</bitbar.image>
-# <bitbar.dependencies>bash</bitbar.dependencies>
+#!/usr/bin/env bash
+## Made for Bitbar https://getbitbar.com/ or Argos https://extensions.gnome.org/extension/1176/argos/ with ❤️
 
-## 1. open Firefox
-## 2. in to URL type about:profiles
-## 3. create profiles
-## 4. put your profile names in to PROFILES variable
-PROFILES="Default Anonimize Private Bank Work"
+shopt -s extglob
 
 ROOTDIR="$(dirname "${BASH_SOURCE[0]}")"
 SELFNAME="$(basename "${BASH_SOURCE[0]}")"
-PATTERN="+($(echo ${PROFILES// /|}))"
-EDITIONS="s d"
+## Icon
+ICON="🦊"
+#ICON=":globe_with_meridians:"
+## Sort output by FF Profiles or FF Editions
+## Firefox profiles = FFP
+## Firefox editions = FFE
+FFSORT="FFP"
+## Detect OS
+PLATFORM="unknown"
+UNAMESTR="$(uname)"
+if [ "$UNAMESTR" == "Linux" ]; then
+  PLATFORM="linux"
+  ## Firefox Stable binary path
+  FFSBIN="/usr/bin/firefox"
+  ## Firefox ESR binary path
+  FFEBIN="/usr/bin/firefox-esr"
+  ## Firefox Developer Edition binary path
+  FFDBIN="/usr/bin/firefox-developer-edition"
+  ## Firefox Nightly binary path
+  FFNBIN="/usr/bin/firefox-nightly"
+  ## Firefox Linux profiles path
+  FFPPATH="$HOME/.mozilla/firefox"
+elif [ "$UNAMESTR" == "Darwin" ]; then
+  PLATFORM="mac"
+  ## Firefox Stable binary path
+  FFSBIN="/Applications/Firefox.app/Contents/MacOS/firefox"
+  ## Firefox ESR binary path
+  FFEBIN="/Applications/Firefox ESR.app/Contents/MacOS/firefox"
+  ## Firefox Developer Edition binary path
+  FFDBIN="/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox"
+  ## Firefox Nightly binary path
+  FFNBIN="/Applications/Firefox Nightly Edition.app/Contents/MacOS/firefox"
+  ## Firefox macOS profiles path
+  FFPPATH="$HOME/Library/Application Support/Firefox/Profiles"
+fi
+## Firefox profiles
+PROFILES="$(find "$FFPPATH" -maxdepth 1 -name "*.*" -type d | awk -F"." '{print $NF}' | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g')"
+if [ ! -f "$FFSBIN" -a ! -f "$FFEBIN" -a ! -f "$FFDBIN" -a ! -f "$FFNBIN" ]; then
+  echo "Install Firefox or edit Binary Paths in script!"
+  exit 1
+fi
+FFBINCOUNTER="0"
+EDITIONS=()
+if [ -f "$FFNBIN" ]; then
+  let FFBINCOUNTER=$FFBINCOUNTER+1
+  EDTN="n"
+  EDITIONS=("${EDITIONS[@]}" "n")
+  open_ffd() {
+    "$FFDBIN" -P "${PROFILE}" -no-remote >/dev/null 2>&1 &
+  }
+fi
+if [ -f "$FFDBIN" ]; then
+  let FFBINCOUNTER=$FFBINCOUNTER+1
+  EDTN="d"
+  EDITIONS=("${EDITIONS[@]}" "d")
+  open_ffd() {
+    "$FFDBIN" -P "${PROFILE}" -no-remote >/dev/null 2>&1 &
+  }
+fi
+if [ -f "$FFEBIN" ]; then
+  let FFBINCOUNTER=$FFBINCOUNTER+1
+  EDTN="e"
+  EDITIONS=("${EDITIONS[@]}" "e")
+  open_ffe() {
+    "$FFEBIN" -P "${PROFILE}" -no-remote >/dev/null 2>&1 &
+  }
+fi
+if [ -f "$FFSBIN" ]; then
+  let FFBINCOUNTER=$FFBINCOUNTER+1
+  EDTN="s"
+  EDITIONS=("${EDITIONS[@]}" "s")
+  open_ffs() {
+    "$FFSBIN" -P "${PROFILE}" -no-remote >/dev/null 2>&1 &
+  }
+fi
 
+PPATTERN="+($(echo ${PROFILES// /|}))"
+EDITIONSX="$(echo ${EDITIONS[@]}|rev)"
+EPATTERN="+($(echo ${EDITIONSX// /|}))"
 
-## Firefox Stable
-open_ffs() {
-  /Applications/Firefox.app/Contents/MacOS/firefox -P "${PROFILE}" >/dev/null 2>&1 &
-}
-
-## Firefox Developer Edition
-open_ffd() {
-  /Applications/Firefox\ Developer\ Edition.app/Contents/MacOS/firefox -P "${PROFILE}" >/dev/null 2>&1 &
-}
-
-bitbar() {
-  echo "🦊"
+ff_profiles() {
+  echo "$ICON"
   echo "---";
   for PROFILE in ${PROFILES}; do
-    echo "$PROFILE | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=s terminal=false"
-    if [ -f "/Applications/Firefox Developer Edition.app/Contents/MacOS/firefox" ]; then
-      echo "----"
-      echo "-- Stable | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=s terminal=false"
-      echo "-- Devel | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=d terminal=false"
+    echo "$PROFILE | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=$EDTN terminal=false"
+    if [ $FFBINCOUNTER -gt 1 ]; then
+      if [ -f "$FFSBIN" ]; then
+        echo "-- STABLE | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=s terminal=false"
+      fi
+      if [ -f "$FFEBIN" ]; then
+        echo "-- ESR | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=e terminal=false"
+      fi
+      if [ -f "$FFDBIN" ]; then
+        echo "-- DEV | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=d terminal=false"
+      fi
+      if [ -f "$FFNBIN" ]; then
+        echo "-- NN | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=n terminal=false"
+      fi
     fi
   done
 }
 
-EDITION="${2:-s}"
-PROFILE="$1"
-case "$PROFILE" in
-  $PATTERN)
-    "open_ff"${EDITION} $PROFILE
-    ;;
-    *) bitbar
-esac
+ff_editions() {
+  echo "$ICON"
+  echo "---";
+  for EDITION in ${EDITIONSX}; do
+    if [ "$EDITION" == "s" ]; then
+      ENAME="Stable"
+    fi
+    if [ "$EDITION" == "e" ]; then
+      ENAME="ESR"
+    fi
+    if [ "$EDITION" == "d" ]; then
+      ENAME="Dev"
+    fi
+    if [ "$EDITION" == "n" ]; then
+      ENAME="Nightly"
+    fi
+    echo "$ENAME"
+    for PROFILE in ${PROFILES}; do
+      echo "-- $PROFILE | bash=$ROOTDIR/$SELFNAME param1=$PROFILE param2=$EDITION terminal=false"
+    done
+  done
+}
+
+if [ "$FFSORT" == "FFP" ]; then
+  PROFILE="$1"
+  EDITION="$2"
+  case "$PROFILE" in
+    $PPATTERN)
+      echo "Running $EDITION $PROFILE"
+      "open_ff"${EDITION} ${PROFILE}
+      ;;
+    *)
+      ff_profiles
+      ;;
+  esac
+fi
+
+if [ "$FFSORT" == "FFE" ]; then
+  PROFILE="$1"
+  EDITION="$2"
+  case "$EDITION" in
+    $EPATTERN)
+      echo "Running $EDITION $PROFILE"
+      "open_ff"${EDITION} ${PROFILE}
+      ;;
+    *)
+      ff_editions
+      ;;
+  esac
+fi
